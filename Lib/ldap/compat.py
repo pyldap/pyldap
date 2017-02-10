378b13c8361a12dc655146f2cd91f28bbe257599
Martin Basti <mbasti@redhat.com>
2017-02-10 19:21:18 +0100

    py3: fix AttributeError with urlopen
    Py3 has urlopen in urllib.requests module, py2 has urlopen in urllib
    
    Fixes: #75

--- Lib/ldap/compat.py
+++ Lib/ldap/compat.py
@@ -6,6 +6,7 @@ if sys.version_info[0] < 3:
     from UserDict import UserDict
     from urllib import quote
     from urllib import unquote as urllib_unquote
+    from urllib import urlopen
     from urlparse import urlparse
 
     def unquote(uri):
@@ -16,3 +17,4 @@ if sys.version_info[0] < 3:
 else:
     from collections import UserDict
     from urllib.parse import quote, unquote, urlparse
+    from urllib.request import urlopen
