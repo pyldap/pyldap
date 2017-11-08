910c858dc891e319dbb455eef2da79ffc169a5c3
Petr Viktorin <pviktori@redhat.com>
2017-11-08 11:29:26 +0100

    ldap.compat: Add a function for re-raising exceptions

--- Lib/ldap/compat.py
+++ Lib/ldap/compat.py
@@ -15,7 +15,28 @@ if sys.version_info[0] < 3:
         uri = uri.encode('ascii')
         unquoted = urllib_unquote(uri)
         return unquoted.decode('utf-8')
+
+    # Old-style of re-raising an exception is SyntaxError in Python 3,
+    # so hide behind exec() so the Python 3 parser doesn't see it
+    exec('''def reraise(exc_type, exc_value, exc_traceback):
+        """Re-raise an exception given information from sys.exc_info()
+
+        Note that unlike six.reraise, this does not support replacing the
+        traceback. All arguments must come from a single sys.exc_info() call.
+        """
+        raise exc_type, exc_value, exc_traceback
+    ''')
+
 else:
     from collections import UserDict
     from urllib.parse import quote, quote_plus, unquote, urlparse
     from urllib.request import urlopen
+
+    def reraise(exc_type, exc_value, exc_traceback):
+        """Re-raise an exception given information from sys.exc_info()
+
+        Note that unlike six.reraise, this does not support replacing the
+        traceback. All arguments must come from a single sys.exc_info() call.
+        """
+        # In Python 3, all exception info is contained in one object.
+        raise exc_value
