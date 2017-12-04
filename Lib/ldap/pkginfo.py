5f30dc392b2a060c7c7aee7e85da9c46b0d36b84
Petr Viktorin <pviktori@redhat.com>
2017-12-04 15:01:16 +0100

    Retire self
    All files are removed, except for README, LICENSE, setup.py and
    requirements.txt
    All these remaining files are now stubs redirecting to python-ldap.

--- Lib/ldap/pkginfo.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-meta attributes for packaging which does not import any dependencies
-"""
-__version__ = '2.5.2'
-__author__ = u'python-ldap project'
-__license__ = 'Python style'
